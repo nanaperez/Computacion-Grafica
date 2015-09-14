f781661ec793c7d9d57df78121defd2a6f667c41
slopezv2 <sebaslv12@hotmail.com>
2015-09-14 13:49:44 -0500

    First part of the projecto: Structure

---
+++ FreakLand2D/src/freakland2d/FreakLand2D.java
@@ -0,0 +1,17 @@
+
+package freakland2d;
+
+/**
+ *
+ * @author Sebastian-PC
+ */
+public class FreakLand2D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
