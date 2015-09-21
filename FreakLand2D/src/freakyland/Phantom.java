c19ebc15391a0c72054e19efc492d4b5b9447258
slopezv2 <sebaslv12@hotmail.com>
2015-09-21 16:15:35 -0500

    Added 3 new phantoms

--- FreakLand2D/src/freakyland/Phantom.java
+++ FreakLand2D/src/freakyland/Phantom.java
@@ -9,7 +9,7 @@ public class Phantom extends Figura {
     private int phantom;
     
     private static final int change = 1;
-    private static final int NUMBEROFPHANTOMS = 6;
+    private static final int NUMBEROFPHANTOMS = 9;
     
     public Phantom(){
         super();
@@ -40,6 +40,15 @@ public class Phantom extends Figura {
                 return img;
             case 5: //BLACK NOPE!
                 img = "/res/img/Spawn.png";
+                return img;
+            case 6:
+                img = "/res/img/Bomberman.png";
+                return img;
+            case 7:
+                img = "/res/img/Joker.png";
+                return img; 
+            case 8:
+                img = "/res/img/Magneto.png";
                 return img;
             default://BUG LEL
                 return "";
