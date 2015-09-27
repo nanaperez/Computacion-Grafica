2f17b9388587f574400a0b27e8a755f5527f4cf8
slopezv2 <sebaslv12@hotmail.com>
2015-09-27 16:14:48 -0500

    It's necessary to chech collisions

--- FreakLand2D/src/freakyland/gui/FreakyLand.java
+++ FreakLand2D/src/freakyland/gui/FreakyLand.java
@@ -7,7 +7,6 @@ import java.awt.AlphaComposite;
 import javax.swing.JLabel;
 import java.awt.Dimension;
 import java.awt.Font;
-
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -17,7 +16,6 @@ import java.awt.event.KeyEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
@@ -26,124 +24,116 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.Timer;
 import org.netbeans.lib.awtextra.AbsoluteConstraints;
 
-public class FreakyLand extends javax.swing.JPanel implements java.awt.event.KeyListener, java.awt.event.ActionListener {
-    
+public class FreakyLand extends javax.swing.JPanel implements java.awt.event.KeyListener {
     private Player player;
     private Phantom phantomTrap;
     private Phantom phantom;
     private Scene scene;
+    // Array to manage phantoms in the screen as value objects
     private ArrayList<Phantom> phantoms;
+    // Array to manage phantoms in the screen as JLabels
     private ArrayList<JLabel> phantomsImg;
     private JLabel playerImg;
     private JLabel sceneImg;
     private JLabel phantomImg;
     private JLabel scoreBoard;
-    private int playerHitBoxX = 100;
-    private int playerHitBoxY = 100;
-    
+    private final int playerHitBoxX = 100;
+    private final int playerHitBoxY = 100;
     private static final int WIDTH = 1000; //WIDTH of the panel.
     private static final int HEIGHT = 700; //HEIGHT of the panel.
     private static final int TIMETOREAD = 2500;
-    
     private ImageIcon backgr;
-    private int score;
-    
     public FreakyLand(){
         initComponents();
     }
-    
     private void initComponents(){
         //JPanel configs and stuff
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(new java.awt.Color(255, 255, 255)); //White screen, why not.
-        setFocusable(true);
+        // Layout to put objects in whatever part
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        
-        //Code n' stuff 
+        //the scene
         scene = new Scene();
-        
         scene.setImg(scene.randomizeScenario());
-        
         player = new Player();
+        // Config to know if pacman hit the phantom
         player.setHitBoxX(playerHitBoxX); //SET HITBOX
         player.setHitBoxY(playerHitBoxY); //SET HITBOX
         playerImg = new JLabel();
+        //upload pacman image
         playerImg.setIcon(new javax.swing.ImageIcon(getClass().getResource(
         player.getImgIcon())));
         playerImg.setPreferredSize(new Dimension(150, 150));
+        //initial Position for Pacman
         player.setPosX(WIDTH/2);
         player.setPosY(HEIGHT - 200);
         playerImg.setLocation(player.getPosX(), player.getPosY());
-        
         phantoms = new ArrayList<>();
         phantomsImg = new ArrayList<>();
-        
+        // show the score
         scoreBoard = new JLabel();
         scoreBoard.setFont(new Font("Serif", Font.PLAIN, 25));
         scoreBoard.setText(Integer.toString(scene.getScore()));
-        
+        //add Pacman to JPanel
        add(playerImg, new AbsoluteConstraints(WIDTH/2, HEIGHT-200, -1, -1));
+       //add score to JPanel
        add(scoreBoard, new AbsoluteConstraints(50, 50, -1, -1));
+       // upload the scene background
        backgr = new javax.swing.ImageIcon(getClass().getResource(
             scene.getImg()));
-       score = 0;
+      
     }
-        
-    
-    
     public static void main(String args[]) {
         FreakyLand panel = new FreakyLand();
-        
-        javax.swing.JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Freakyland: Old School");
+        // Dimensions for the frame
         frame.setSize(WIDTH, HEIGHT);
-        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(panel);
+        //To listen events
         frame.setFocusable(true);
         frame.addKeyListener(panel);
         frame.setResizable(false);
-        frame.setVisible(true);     
+        frame.setVisible(true);
         panel.startGame();
     }
-
     @Override
     public void keyTyped(KeyEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet."); 
+        // Not necessary to do something here
     }
-
+    /**
+     * Method to manage key pressed
+     * @param e KeyEvent   
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         int click = e.getKeyCode();
+        // if key is left arrow or a
         if(KeyEvent.VK_LEFT == click||KeyEvent.VK_A == click){
             player.moveLeft();
+            // if key is right arrow or d
         } else if (KeyEvent.VK_RIGHT == click ||KeyEvent.VK_D == click ){
             player.moveRight();
         }
         playerImg.setLocation(player.getPosX(), player.getPosY());
     }
-
     @Override
     public void keyReleased(KeyEvent e) {
- 
+        //Not necessary to do
     }
-    
     int check;
-    Random rn = new Random();
-    
+    Random rn = new Random(System.currentTimeMillis());
     @Override
     public void paintComponent(Graphics g) {
-       
        super.paintComponent(g); 
        g.drawImage(backgr.getImage(), 0, 0, null);
-       
+       // Trick to control phatoms spawn wit out interruptions
        check = Math.abs(rn.nextInt())%700;
-       
        if(check==0){
             spawnPhantom();
        }
-       
        //Check for colision
        int playerPosX = player.getPosX();
        int playerPosY = player.getPosY();
@@ -151,49 +141,36 @@ public class FreakyLand extends javax.swing.JPanel implements java.awt.event.Key
        for(int i = 0; i<phantomsImg.size();i++){
            phantomPosX = phantoms.get(i).getPosX();
            phantomPosY = phantoms.get(i).getPosY();
-           if(Math.abs(playerPosX - phantomPosX)<=player.getHitBoxX()
-                   && Math.abs(playerPosY - phantomPosY)<=player.getHitBoxY()){
-               
+           if(Math.abs(playerPosX - phantomPosX)<=player.getHitBoxX()-30
+                   && Math.abs(playerPosY - phantomPosY)<=player.getHitBoxY()-50){
                final JLabel phantomTemp = phantomsImg.get(i);
                if(phantoms.get(i).getType()==phantomTrap.getType()){
                    try {
-                       //GG
-                       
+                       // Add points to score
                        scene.addPoint();
                        scoreBoard.setText(Integer.toString(scene.getScore()));
-                       //TODO: INCREASE SIZE OF GHOST
-                       //phantomsImg.get(i).setSize(phantomTrap.getSizeX()*2, phantomTrap.getSizeY()*2);
+                       // Read again phantom image to resize
                        BufferedImage myPicture = ImageIO.read(getClass().getResource(phantoms.get(i).getImg()));
-                       
+                       // set Icon with a bigger image 
                        phantomsImg.get(i).setIcon(new ImageIcon(resizeToBig(myPicture, 400, 400)));
-                       //phantomsImg.get(i).setLocation(WIDTH/2, HEIGHT/2);
-
-                       //validate();
-                       //repaint();
                    } catch (IOException ex) {
                        Logger.getLogger(FreakyLand.class.getName()).log(Level.SEVERE, null, ex);
                    }
                } else {
                    //LOL N00B
-                   
+                   // when you fail
                    scene.deductPoint();
-                   scoreBoard.setText(Integer.toString(scene.getScore()));
-                   //TODO: DECREASE SIZE OF GHOST
-                   /*BufferedImage myPicture;
-                   try {
-                       myPicture = ImageIO.read(getClass().getResource(phantoms.get(i).getImg()));
-                       phantomsImg.get(i).setIcon(new ImageIcon(resizeToBig(myPicture, 50, 50)));
-                   } catch (IOException ex) {
-                       Logger.getLogger(FreakyLand.class.getName()).log(Level.SEVERE, null, ex);
+                   if(scene.getScore() < -30){
+                       showLostMessage();
                    }
-                     */  
+                   scoreBoard.setText(Integer.toString(scene.getScore()));  
                }
                
                new java.util.Timer().schedule( 
                     new java.util.TimerTask() {
                     @Override
                     public void run() {
-                        // your code here
+                        // remove phantom after Pacman hit it
                         remove(phantomTemp);
                     }
                 }, 
@@ -206,17 +183,19 @@ public class FreakyLand extends javax.swing.JPanel implements java.awt.event.Key
            }
            playerImg.setLocation(player.getPosX(), player.getPosY());
        }
-       
        movePhantoms();
        repaint();
-        
-        
     }
-    
+    /**
+     * Method to resize an image
+     * @param originalImage
+     * @param biggerWidth
+     * @param biggerHeight
+     * @return image with the specified width and height
+     */
     private Image resizeToBig(Image originalImage, int biggerWidth, int biggerHeight) {
-    int type = BufferedImage.TYPE_INT_ARGB;
-
-BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
+        int type = BufferedImage.TYPE_INT_ARGB;
+        BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
 	Graphics2D g = resizedImage.createGraphics();
 	g.drawImage(originalImage, 0, 0, biggerWidth,  biggerHeight, null);
 	g.dispose();	
@@ -233,7 +212,9 @@ BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
     public void startGame(){
         showMessage();
     }
-    
+    /**
+     * Method to show the message with the information about the phantom to "eat" with pacman
+     */
     private void showMessage(){ 
         phantomTrap =  new Phantom();
         String imgPath = phantomTrap.randomizePhantom();
@@ -252,20 +233,25 @@ BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
         frame.remove(ins);
         repaint();
     }
-    
-
-    
+    /**
+     * Method to show lost message and deactivate keyListener
+     */
+    private void showLostMessage(){
+        Lost ins = new Lost();
+        JFrame frame = (JFrame)this.getParent().getParent().getParent();
+        frame.add(ins,new org.netbeans.lib.awtextra.AbsoluteConstraints(WIDTH/2- 175, HEIGHT/2 - 200, -1, -1));
+        ins.setVisible(true);
+        frame.removeKeyListener(this);
+        repaint();
+    }
     private void spawnPhantom(){
         phantom = new Phantom();
         phantom.setPosX(phantom.randomizePosX(WIDTH));
-        phantom.setPosY(-100);
-        
+        phantom.setPosY(-100);   
         phantomImg = new JLabel();
         phantomImg.setIcon(new ImageIcon(getClass().getResource(
         phantom.randomizePhantom())));
-        
         add(phantomImg,new AbsoluteConstraints(phantom.getPosX(), phantom.getPosY(), -1, -1));
-        
         phantoms.add(phantom);
         phantomsImg.add(phantomImg);
         validate();
@@ -275,7 +261,6 @@ BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
     
     int i = 0;
     private void movePhantoms(){
-        
         if(i%5==0){
             for(int j = 0; j<phantomsImg.size();j++){
                 
@@ -290,21 +275,23 @@ BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
                 } catch (IOException ex) {
                     Logger.getLogger(FreakyLand.class.getName()).log(Level.SEVERE, null, ex);
                 }
-
                }
                 phantomsImg.get(j).setLocation(phantoms.get(j).getPosX(), phantoms.get(j).getPosY());
-                if(phantomsImg.get(j).getY() >= HEIGHT){
+                if(phantoms.get(j).getPosY() >= HEIGHT){
                     this.remove(phantomsImg.get(j));
                     phantomsImg.remove(j);
                     phantoms.remove(j);
                 }
-                //validate();
-                
             }
         }
         i=i+1;
     }
-    
+    /**
+     * Method to rotate images
+     * @param in
+     * @param degrees
+     * @return BufferedImage with the rotated image
+     */
     private BufferedImage rotate(BufferedImage in, int degrees){
         BufferedImage bufferedImage;
         AffineTransform tx = new AffineTransform();
@@ -314,11 +301,4 @@ BufferedImage resizedImage = new BufferedImage(biggerWidth, biggerHeight, type);
         bufferedImage = op.filter(in, null);
         return bufferedImage;
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-
 }
